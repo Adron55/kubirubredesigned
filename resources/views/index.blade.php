c868f2b7813feb84616ec5c0a20a13a824a49808
Adron <anarmemmedli55@gmail.com>
2019-09-03 18:52:18 +0400

    tawk.to plugin added

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -423,6 +423,33 @@
 // document ready
     });
 </script>
+<!--Start of Tawk.to Script-->
+
+<script type="text/javascript">
+
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+
+
+    (function(){
+
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+
+        s1.async=true;
+
+        s1.src='https://embed.tawk.to/5d6a7f69eb1a6b0be60a4ee6/default';
+
+        s1.charset='UTF-8';
+
+        s1.setAttribute('crossorigin','*');
+
+        s0.parentNode.insertBefore(s1,s0);
+
+    })();
+
+</script>
+
+<!--End of Tawk.to Script-->
+
 @stop
 </body>
 </html>
